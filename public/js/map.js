89d8b791aea80214cfdf5ca55aae428241c17d42
SamPetherick1898 <fuentessamuelignacio@gmail.com>
2022-07-13 18:33:55 -0500

    mapas acabados

--- public/js/map.js
+++ public/js/map.js
@@ -6,9 +6,8 @@ let map1 = new mapboxgl.Map({
     container: 'map1',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [2.174308,41.402419],
-    zoom: 15
+    zoom: 12
 })
-
 
 //Map Berlin
 
@@ -16,9 +15,8 @@ let map2 = new mapboxgl.Map({
     container: 'map2',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [13.378357,52.515742],
-    zoom: 15
+    zoom: 12
 })
-
 
 //Map Tokio
 
@@ -26,9 +24,8 @@ let map3 = new mapboxgl.Map({
     container: 'map3',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [139.887165,35.62695],
-    zoom: 15
+    zoom: 12
 })
-
 
 //Place marker Barcelona
 
@@ -36,7 +33,7 @@ let element1 = document.createElement('div')
 element1.className = 'marker1'
 
 element1.addEventListener('click', ()=>{
-    window.alert('Nuestra sucursal Caffé Quimera - Barcelona')
+    alert('Nuestra sucursal Caffé Quimera - Barcelona')
 })
 
 let marker1 = new mapboxgl.Marker(element1)
@@ -53,7 +50,7 @@ let element2 = document.createElement('div')
 element2.className = 'marker2'
 
 element2.addEventListener('click', ()=>{
-    window.alert('Nuestra sucursal Caffé Quimera - Puerta de Brandeburgo')
+    alert('Nuestra sucursal Caffé Quimera - Puerta de Brandeburgo')
 })
 
 let marker2 = new mapboxgl.Marker(element2)
@@ -70,7 +67,7 @@ let element3 = document.createElement('div')
 element3.className = 'marker3'
 
 element3.addEventListener('click', ()=>{
-    window.alert('Nuestra sucursal Caffé Quimera - Tokyo DisneySea')
+    alert('Nuestra sucursal Caffé Quimera - Tokyo DisneySea')
 })
 
 let marker3 = new mapboxgl.Marker(element3)
@@ -80,3 +77,31 @@ let marker3 = new mapboxgl.Marker(element3)
     bearing: 90
 })
 .addTo(map3)
+
+//Maps controls and navigation
+
+map1.addControl(new mapboxgl.NavigationControl());
+map1.addControl(new mapboxgl.FullscreenControl());
+
+map2.addControl(new mapboxgl.NavigationControl());
+map2.addControl(new mapboxgl.FullscreenControl());
+
+map3.addControl(new mapboxgl.NavigationControl());
+map3.addControl(new mapboxgl.FullscreenControl());
+
+//CC maps
+
+map1.on('mousemove', function (e) {
+    document.getElementById('coordenadas1').innerHTML =
+        JSON.stringify(e.lngLat);
+});
+
+map2.on('mousemove', function (e) {
+    document.getElementById('coordenadas2').innerHTML =
+        JSON.stringify(e.lngLat);
+});
+
+map3.on('mousemove', function (e) {
+    document.getElementById('coordenadas3').innerHTML =
+        JSON.stringify(e.lngLat);
+});
